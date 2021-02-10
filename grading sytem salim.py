c9531f3133d4b699cdaf4c967e86b95054e36b22
salimmukhuana <78488009+salimmukhuana@users.noreply.github.com>
2021-02-10 16:40:25 +0300

    Add files via upload

---
+++ grading sytem salim.py
@@ -0,0 +1,38 @@
+print('Hello, world!')
+i=9
+if i>10:
+    print('Salim')
+else:
+    print('False')
+print("enter number")
+a= int(input())
+if a%2==0:
+   print("even")
+else:
+    print("odd")
+
+      print("enter number")
+a=int(input("enter marks for a"))
+b=int(input("enter marks for b"))
+c=int(input("enter marks for c"))
+d=int(input("enter marks for d"))
+e=int(input("enter marks for e"))
+avg=int((a+b+c+d+e)/5)
+if (avg>=70 and avg<=100):
+    print("A")
+elif(avg>=60 and avg<=69):
+      print("B")
+elif(avg>=50 and avg<=59):
+      print("c")
+elif(avg>=40 and avg<=49):
+      print("D")
+else:
+        print("fail")
+
+
+
+
+
+
+
+
